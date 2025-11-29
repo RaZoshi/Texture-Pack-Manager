819b42d89e359bbaf91d56c8b003c92f879e7050
razoshi <141675594+RaZoshi@users.noreply.github.com>
2025-11-29 20:00:33 +0300

    Update and rename Inject-Version to Inject-Version.js

---
+++ Inject-Version.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         MooMoo.io Texture-Pack Manager (Inject Version)
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-27
+// @description  https://github.com/RaZoshi
+// @author       razoshi
+// @match        *://*.moomoo.io/*
+// @match        *://*moomoo.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=moomoo.io
+// @grant        none
+// ==/UserScript==
+
+/*
+    Author: RaZoshi
+    Version: 2024-04-27
+    GitHub: https://github.com/RaZoshi
+*/
+
+(async function() {
+    "use strict"
+
+    await fetch("").then((res) => res.text()).then((text) => {
+        eval(text);
+    });
+})();
